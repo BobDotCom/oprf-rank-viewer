38bb84374b291e1f6af92709ba7c6dbe5621fa56
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2023-12-07 15:16:09 -0600

    feat: migrate to new vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import {Analytics} from '@vercel/analytics/react';
 import type {Metadata} from 'next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
